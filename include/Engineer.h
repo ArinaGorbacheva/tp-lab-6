8a9234cd0f4e0cacc03edcbf7e1c14d27b3dc889
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-06-06 23:13:24 +0300

    Update Engineer.h

--- include/Engineer.h
+++ include/Engineer.h
@@ -1,48 +1,49 @@
 #pragma once
 #include "Employee.h"
 #include "WorkTime.h"
-#include "Project.h"
+#include "Project.h" 
+//ставка с почасовой оплатой и бонусы за участие в проектах
 class Engineer : public Employee, public WorkTime, public Project {
 protected:
-	int base;		
-	double deposit;	
-	string project;	
-	int budget;		
+	int base;	//ставка	
+	double deposit;	//вклад
+	string project;	//проект
+	int budget;	//бюджет	
 
 public:
-	Engineer() : Employee() {}; 
-	int WorkTimePaymentMethod(int work_time, int base) override {
+	Engineer() : Employee() {}; //конструктор по умолчанию наследуемый у Employee
+	int WorkTimePaymentMethod(int work_time, int base) override { //рассчитать зп по ставке
 		int payment = work_time * base;
 		return payment;
 	}
 
 	
-	int ProjectPaymentMethod(int budget, double deposit) override {
+	int ProjectPaymentMethod(int budget, double deposit) override { //зп от учетом бонуса от выполняемого проекта
 		int bonus = budget * deposit;
 		return bonus;
 	}
 
-	void setBase(int base) {
-		this->base = base;
+	void setBase(int base) { //установить ставку
+		this->base = base; 
 	}
 
-	int getBase() const {
+	int getBase() const { //получить тек ставку
 		return base;
 	}
 
-	void setDeposit(double depos) {
+	void setDeposit(double depos) { //установить депозит
 		this->deposit = depos;
 	}
 
-	double getDeposit() {
+	double getDeposit() { //получить тек депозит
 		return deposit;
 	}
 
-	void setProject(string project_name) {
+	void setProject(string project_name) { //задать проект
 		this->project = project_name;
 	}
 
-	string getProject() {
+	string getProject() { //получить проект
 		return project;
 	}
 
