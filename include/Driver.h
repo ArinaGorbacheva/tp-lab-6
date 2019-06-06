b2fb2a639b869ddd48329f8562b720a6682fad7d
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-06-06 23:02:10 +0300

    Update Driver.h

--- include/Driver.h
+++ include/Driver.h
@@ -4,18 +4,18 @@
 class Driver : public Personal
 {
 public:
-	Driver(int id, string name, int worktime, int base) { 
+	Driver(int id, string name, int worktime, int base) { //конструктор с параметрами (id, фио, ставка, отработанное время, зп)
 		this->id = id;				
 		this->name = name;			
 		this->base = base;			
 		this->worktime = worktime;	
 		calculatePayment();	
 	}
-	void calculatePayment() override
+	void calculatePayment() override //метод назначения зп
 	{
 		this->payment = WorkTimePaymentMethod(this->worktime, this->base); 
 	}
-	int WorkTimePaymentMethod(int worktime, int base) override
+	int WorkTimePaymentMethod(int worktime, int base) override //метод расчета зп
 	{
 		this->payment = worktime * base;
 		return payment;
