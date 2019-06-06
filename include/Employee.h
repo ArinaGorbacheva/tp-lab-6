c07f5c33b3a45bf859ffb7b5de0cd96ae8a8f2b6
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-06-06 23:07:37 +0300

    Update Employee.h

--- include/Employee.h
+++ include/Employee.h
@@ -2,7 +2,7 @@
 #include <string>
 
 using namespace std;
-
+//основной родительский класс для всех методов
 class Employee
 {
 protected:
@@ -12,42 +12,42 @@ protected:
 	int worktime;	
 	int payment;	
 
-public:
+public: //описываем методы
 	
-	Employee() {}	
+	Employee() {}	//конструктор по умолчанию
 
-	Employee(int id, string name, int worktime, int payment) { 
+	Employee(int id, string name, int worktime, int payment) { //конструктор с параметрами (id, фио, отработанное время)
 		this->id = id;
 		this->name = name;
 		this->worktime = worktime;
 	}
-	virtual void calculatePayment() = 0;
+	virtual void calculatePayment() = 0; //унив метод установки зп
 
-	void setID(int id) {
+	void setID(int id) { //присвоить  id
 		this->id = id;
 	}
 
-	int getID() const {
+	int getID() const { //получить  id
 		return id;
 	}
 
-	void setFIO(string name) {
+	void setFIO(string name) { //назначить фио
 		this->name = name;
 	}
 
-	string getFIO() const {
+	string getFIO() const { //получить фио
 		return name;
 	}
 
-	void setWorkTime(int worktime) {
+	void setWorkTime(int worktime) { //присвоить отработанное время
 		this->worktime = worktime;
 	}
 
-	int getWorkTime() const {
+	int getWorkTime() const { //получить отработанное время
 		return worktime;
 	}
 
-	int getPaymentTime() const {
+	int getPaymentTime() const { //получить рабочее время работника
 		return payment;
 	}
 
